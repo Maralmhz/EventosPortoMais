652672cfa912a7026bd16413f37b5f214cab2368
Maralmhz <maralmhz@gmail.com>
2026-03-17 09:18:16 -0300

    Create SimuladorCota.jsx

---
+++ src/pages/SimuladorCota/SimuladorCota.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SimuladorCota = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Simulador de Cota</h1>
+      <p>FIPE: ...</p>
+      <p>Associação: ...</p>
+      <p>Cota: ...</p>
+    </div>
+  );
+};
+
+export default SimuladorCota;
